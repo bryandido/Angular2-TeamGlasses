7c62865161fbf56ee8fccbfcaefa77b011c5906f
bryandido <balanyan.kyaw@sjsu.edu>
2017-04-23 12:49:36 -0700

    Saved for ryan

---
+++ src/app/services/product/product.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ProductService{
+
+  constructor(private _http:Http){
+    console.log('Product Service Ready...');
+  }
+// Change URL depending on where you're hosting it.
+  getAll(){
+    return this._http.get('http://10.250.190.152/teamglasses/teamglasses-api/api.php/product?&transform=1').map(res => res.json());
+  }
+  getById(id:number){
+    return this._http.get('http://10.250.190.152/teamglasses/teamglasses-api/api.php/product/' + id + '&transform=1').map(res => res.json());
+  }
+  getByName(email:string){
+    return this._http.get('http://10.250.190.152/teamglasses/teamglasses-api/api.php/product?filter[]=email,eq,' + email + '&transform=1').map(res => res.json());
+  }
+  create(user:string){
+    return this._http.post('http://10.250.190.152/teamglasses/teamglasses-api/api.php/user/', user).map(res => res.json());
+  }
+  update(user:string){
+    //return this._http.put('http://10.50.1.169/teamglasses-api/api.php/user/' + user.user_id, user);
+  }
+  delete(id: string){
+    return this._http.delete('http://10.250.190.152/teamglasses/teamglasses-api/api.php/user/' + id).map(res => res.json());
+  }
+}
