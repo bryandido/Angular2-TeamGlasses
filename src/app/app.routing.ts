866af6a2dde75567a820da3449544cea98e25020
hyesungko <kohs0429@gmail.com>
2017-04-23 12:45:45 -0700

    Routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,13 +1,15 @@
-import {ModuleWithProviders} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-import {SearchComponent} from './components/search/search.component';
-import {CartComponent} from './components/cart/cart.component';
-import {CheckoutComponent} from './components/cart/checkout/checkout.component';
+import { SearchComponent } from './components/search/search.component';
+import { CartComponent } from './components/cart/cart.component';
+import { CheckoutComponent } from './components/cart/checkout/checkout.component';
+import { Product_DetailComponent } from './components/product_detail/product_detail.component';
+import { PageNotFoundComponent } from './components/page_not_found/page_not_found.component';
 
 const appRoutes: Routes = [
   {
-    path:'',
+    path:'search',
     component:SearchComponent
   },
   {
@@ -17,6 +19,19 @@ const appRoutes: Routes = [
   {
     path:'cart/checkout',
     component:CheckoutComponent
+  },
+  {
+    path:'product_detail',
+    component:Product_DetailComponent
+  },
+  {
+    path: '',
+    redirectTo: 'search',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
   }
 ];
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
---
+++ src/app/components/product_detail/product_detail.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'product_detail',
+  templateUrl: 'product_detail.component.html',
+})
+export class Product_DetailComponent {}
