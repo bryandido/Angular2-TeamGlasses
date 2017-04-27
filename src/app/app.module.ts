908010fa35f1bfe22b32898a44229d837a5cb8f8
Ragnhild Marie Holen <marieh@Ragnhilds-MacBook-Pro.local>
2017-04-26 21:38:58 -0700

    Updated design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,18 @@ import { HttpModule } from '@angular/http';
 import { SearchComponent } from './components/search/search.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ProdDetailComponent } from './components/product_detail/product_detail.component';
 import { CartComponent } from './components/cart/cart.component';
 import { CheckoutComponent } from './components/cart/checkout/checkout.component';
 import { AboutComponent } from './components/about/about.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
+import { FAQComponent } from './components/faq/faq.component';
+import { ReturnComponent } from './components/return/return.component';
+import { ShippingComponent } from './components/shipping/shipping.component';
+import { SupportComponent } from './components/support/support.component';
 import {routing} from './app.routing';
 import {ArrayPipe} from './pipes/array.pipe'
 
@@ -18,9 +27,18 @@ import {ArrayPipe} from './pipes/array.pipe'
                   SearchComponent,
                   NavbarComponent,
                   FooterComponent,
+                  RegisterComponent,
+                  LoginComponent,
+                  ProfileComponent,
+                  ProdDetailComponent,
                   CartComponent,
                   CheckoutComponent,
                   AboutComponent,
+                  ConfirmationComponent,
+                  FAQComponent,
+                  ReturnComponent,
+                  ShippingComponent,
+                  SupportComponent,
                   ArrayPipe ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,14 +2,39 @@ import {ModuleWithProviders} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 
 import {SearchComponent} from './components/search/search.component';
+import {RegisterComponent} from './components/register/register.component';
+import {LoginComponent} from './components/login/login.component';
+import {ProfileComponent} from './components/profile/profile.component';
+import {ProdDetailComponent} from './components/product_detail/product_detail.component';
 import {CartComponent} from './components/cart/cart.component';
 import {CheckoutComponent} from './components/cart/checkout/checkout.component';
 import {AboutComponent} from './components/about/about.component';
+import {ConfirmationComponent} from './components/confirmation/confirmation.component';
+import {FAQComponent} from './components/faq/faq.component';
+import {ReturnComponent} from './components/return/return.component';
+import {ShippingComponent} from './components/shipping/shipping.component';
+import {SupportComponent} from './components/support/support.component';
 
 const appRoutes: Routes = [
   {
     path:'',
     component:SearchComponent
+  },
+  {
+    path:'register',
+    component:RegisterComponent
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'profile',
+    component:ProfileComponent
+  },
+  {
+    path:'product_detail',
+    component:ProdDetailComponent
   },
   {
     path:'cart',
@@ -22,6 +47,26 @@ const appRoutes: Routes = [
   {
     path:'about',
     component:AboutComponent
+  },
+  {
+    path:'confirmation',
+    component:ConfirmationComponent
+  },
+  {
+    path:'faq',
+    component:FAQComponent
+  },
+  {
+    path:'return',
+    component:ReturnComponent
+  },
+  {
+    path:'shipping',
+    component:ShippingComponent
+  },
+  {
+    path:'support',
+    component:SupportComponent
   }
 ];
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
