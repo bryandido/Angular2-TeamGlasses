173d8186d081575aca8adb77457fb9831fd25277
Ragnhild Marie Holen <marieh@Ragnhilds-MacBook-Pro.local>
2017-04-26 22:14:49 -0700

    style changes

---
+++ src/app/components/confirmation/confirmation.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'confirmation',
+  templateUrl: 'confirmation.component.html',
+})
+export class ConfirmationComponent  {}
---
+++ src/app/components/return/return.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'return',
+  templateUrl: 'return.component.html',
+})
+export class ReturnComponent  {}
