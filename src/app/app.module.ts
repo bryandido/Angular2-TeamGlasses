5e64373b390f693a9c3ea5b8006ebf6ee91373a6
hyesungko <kohs0429@gmail.com>
2017-05-13 02:22:51 -0700

    Filter working'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent }  from './app.component';
 import { HttpModule } from '@angular/http';
 import { CommonModule } from "@angular/common"
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
@@ -16,21 +17,17 @@ import { ConfirmationComponent }from './components/confirmation/confirmation.com
 import { PageNotFoundComponent } from './components/page_not_found/page_not_found.component';
 import { ContentComponent } from './components/content/content.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { SearchBarComponent } from './components/searchbar/searchbar.component';
 import { DetailModalComponent } from './components/detail_modal/detail_modal.component';
 import { routing } from './app.routing';
 
 import { SearchLaptopComponent } from './components/searchLaptop/searchLaptop.component';
-import { ContentLaptopComponent } from './components/contentLaptop/contentLaptop.component';
 import { SearchTabletComponent } from './components/searchTablet/searchTablet.component';
-import { ContentTableComponent } from './components/contentTablet/contentTablet.component';
 import { SearchTvComponent } from './components/searchTv/searchTv.component';
-import { ContentTvComponent } from './components/contentTv/contentTv.component';
 
 import { ArrayPipe } from './pipes/array.pipe'
 
 @NgModule({
-  imports:      [ BrowserModule, routing, FormsModule, HttpModule, CommonModule],
+  imports:      [ BrowserModule, routing, FormsModule, HttpModule, CommonModule, Ng2SearchPipeModule],
   declarations: [ AppComponent,
                   SearchComponent,
                   NavbarComponent,
@@ -43,13 +40,9 @@ import { ArrayPipe } from './pipes/array.pipe'
                   PageNotFoundComponent,
                   ContentComponent,
                   FooterComponent,
-                  SearchBarComponent,
                   SearchTvComponent,
                   SearchLaptopComponent,
                   SearchTabletComponent,
-                  ContentTvComponent,
-                  ContentTableComponent,
-                  ContentLaptopComponent,
                   DetailModalComponent,
                   ArrayPipe ],
   bootstrap:    [ AppComponent ]
--- src/app/components/searchbar/searchbar.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  moduleId: module.id,
-  selector: 'searchbar',
-  templateUrl: 'searchbar.component.html',
-})
-export class SearchBarComponent {}
