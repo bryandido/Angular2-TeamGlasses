1d28bba298ba6faed6beb71d605e2964470f1bd5
hyesungko <kohs0429@gmail.com>
2017-05-15 17:49:00 -0700

    Log in and off working

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   moduleId: module.id,
   selector: 'navbar',
   templateUrl: 'navbar.component.html',
 })
-export class NavbarComponent  {}
+export class NavbarComponent implements OnInit {
+  loggedIn: Boolean;
+
+  ngOnInit(){
+    console.log(window.sessionStorage.getItem('userID'));
+    if(window.sessionStorage.getItem('userID') == null){
+      this.loggedIn = false;
+      console.log("is null");
+    }
+    else {
+      this.loggedIn = true;
+      console.log("is not null");
+    }
+  }
+  logoff(){
+    window.sessionStorage.removeItem('userID');
+    location.reload();
+  }
+}
--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { UserService } from '../../services/user/user.service';
 
 @Component({
   moduleId: module.id,
   selector: 'profile',
   templateUrl: 'profile.component.html',
 })
-export class ProfileComponent  {}
+export class ProfileComponent  {
+posts:any=[];
+userid:number=0;
+firstname:string;
+lastname:string;
+email:string;
+  constructor(private _userService: UserService){
+    var a=JSON.parse(window.sessionStorage.getItem("userID"));
+    a = String[a];
+    console.log(a);
+    this.userid = Number[window.sessionStorage.getItem('userID')];
+    console.log(this.userid);
+    this._userService.getById(2).subscribe(posts => {
+      this.posts = posts;
+      console.log(posts);
+      this.firstname=this.posts.first_name;
+      this.lastname=this.posts.last_name;
+      this.email=this.posts.email;
+      console.log(this.firstname);
+    });
+    console.log(this.posts);
+  }
+
+}
