285c8ed468d7b07091ce848697d907f9954458d9
bryandido <balanyan.kyaw@sjsu.edu>
2017-05-18 16:59:17 -0700

    Final Product [last modified by Ryan]

--- src/app/services/transaction/transaction.service.ts
+++ src/app/services/transaction/transaction.service.ts
@@ -10,19 +10,19 @@ export class TransactionService{
   }
 // Change URL depending on where you're hosting it.
   getAll(){
-    return this._http.get('http://10.250.190.152/teamglasses/teamglasses-api/api.php/transaction?&transform=1').map(res => res.json());
+    return this._http.get('http://10.50.1.169/teamglasses/teamglasses-api/api.php/transaction?&transform=1').map(res => res.json());
   }
   getByUserId(id:number){
-    return this._http.get('http://10.250.190.152/teamglasses/teamglasses-api/api.php/transaction?filter[]=user_id,eq,' + id + '&transform=1').map(res => res.json());
+    return this._http.get('http://10.50.1.169/teamglasses/teamglasses-api/api.php/transaction?filter[]=user_id,eq,' + id + '&transform=1').map(res => res.json());
   }
   getById(id:number){
-    return this._http.get('http://10.250.190.152/teamglasses/teamglasses-api/api.php/transaction?filter[]=id,eq,' + id + '&transform=1').map(res => res.json());
+    return this._http.get('http://10.50.1.169/teamglasses/teamglasses-api/api.php/transaction?filter[]=id,eq,' + id + '&transform=1').map(res => res.json());
   }
   getByName(email:string){
-    return this._http.get('http://10.250.190.152/teamglasses/teamglasses-api/api.php/transaction?filter[]=email,eq,' + email + '&transform=1').map(res => res.json());
+    return this._http.get('http://10.50.1.169/teamglasses/teamglasses-api/api.php/transaction?filter[]=email,eq,' + email + '&transform=1').map(res => res.json());
   }
   getByType(type:string){
-    return this._http.get('http://10.250.190.152/teamglasses/teamglasses-api/api.php/transaction?filter[]=type,eq,' + type + '&transform=1').map(res => res.json());
+    return this._http.get('http://10.50.1.169/teamglasses/teamglasses-api/api.php/transaction?filter[]=type,eq,' + type + '&transform=1').map(res => res.json());
   }
   postTransaction(transaction:any){
     var json = JSON.stringify(transaction);
@@ -30,9 +30,9 @@ export class TransactionService{
     var headers = new Headers();
     headers.append('Content-Type','application/json');
     console.log(json);
-    return this._http.post('http://10.250.190.152/teamglasses/teamglasses-api/api.php/transaction/', params, {headers: headers}).map(res => res.json());
+    return this._http.post('http://10.50.1.169/teamglasses/teamglasses-api/api.php/transaction/', params, {headers: headers}).map(res => res.json());
   }
   delete(id: string){
-    return this._http.delete('http://10.250.190.152/teamglasses/teamglasses-api/api.php/transaction/' + id).map(res => res.json());
+    return this._http.delete('http://10.50.1.169/teamglasses/teamglasses-api/api.php/transaction/' + id).map(res => res.json());
   }
 }
