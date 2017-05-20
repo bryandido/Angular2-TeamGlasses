f62ab0f40ead6de8afc058f2a292a65e15abc99f
RagnhildMarie <rm@oslo.net>
2017-05-19 23:39:21 -0700

    finalized design

---
+++ src/app/components/faq/faq.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'faq',
+  templateUrl: 'faq.component.html',
+})
+export class FAQComponent  {}
---
+++ src/app/components/shipping/shipping.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'shipping',
+  templateUrl: 'shipping.component.html',
+})
+export class ShippingComponent  {}
